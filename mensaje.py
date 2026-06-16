edab1b7460b8544af32787b2e2f52658c2cc694c
María <maria.arrobagomezjover@usp.ceu.es>
2026-06-16 12:08:23 +0200

    Test 4 ok

--- mensaje.py
+++ mensaje.py
@@ -1,8 +1,13 @@
+import random
+
 def procesar(comando, frases):
     comando = comando.strip()
 
     if comando == 'TOTAL':
         lineas = len(frases)
         return f'TOTAL: {len(frases)} FRASES {lineas} LINEAS'
+
+    if comando == 'FRASE':
+        return 'FRASE: ' + random.choice(frases)
 
     return 'ERROR'
