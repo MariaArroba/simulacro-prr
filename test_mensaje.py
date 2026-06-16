3ff4bbdaa50ccc4abba9aac22d00bb7def398b23
María <maria.arrobagomezjover@usp.ceu.es>
2026-06-16 11:06:49 +0200

    Test 2

--- test_mensaje.py
+++ test_mensaje.py
@@ -7,6 +7,11 @@ class TestMensaje(unittest.TestCase):
         frases = ['una frase']
         respuesta = procesar('HOLA', frases)
         self.assertEqual(respuesta, 'ERROR')
+    def test_total_devuelve_numero_de_frases(self):
+        frases = ['uno', 'dos', 'tres']
+        respuesta = procesar('TOTAL', frases)
+        self.assertEqual(respuesta, 'TOTAL: 3 FRASES')
+
 
 if __name__ == '__main__':
     unittest.main()
